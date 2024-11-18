24344056df0b5d5235dab3ac709d973cda0bc83b
Vanessa Noemi Zurita Montaguano <67701176+vanessazurita@users.noreply.github.com>
2024-11-18 17:53:55 -0500

    Info

--- deber1g1/lib/screens/info.dart
+++ deber1g1/lib/screens/info.dart
@@ -7,10 +7,30 @@ class InfoScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Información'),
       ),
-      body: Center(
-        child: Text(
-          'Esta es la pantalla de información',
-          style: TextStyle(fontSize: 24),
+       body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(height: 20),
+            Image.network(
+              'https://upload.wikimedia.org/wikipedia/commons/1/17/Google-flutter-logo.png', // URL de la imagen
+              width: 150,
+              height: 150,
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Desarrollo de Aplicaciones Móviles\nGrupo 1',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              'Daniel Armas\nLeonardo De La Cadena\nJosué Merino\nVanessa Zurita',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 18),
+            ),
+          ],
         ),
       ),
     );
