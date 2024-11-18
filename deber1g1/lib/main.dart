1045304cd3edd713789d033cd158d476c16f2ffb
Vanessa Noemi Zurita Montaguano <67701176+vanessazurita@users.noreply.github.com>
2024-11-18 18:46:12 -0500

    Cambio main

--- deber1g1/lib/main.dart
+++ deber1g1/lib/main.dart
@@ -12,6 +12,8 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -25,30 +27,51 @@ class MyApp extends StatelessWidget {
 }
 
 class HomeScreen extends StatelessWidget {
+  // List of exercises with a color for each icon
   final List<Exercise> exers = [
-    Exercise(name: 'Ejercicio 1', icon: Icons.boy, route: OneScreen()),
     Exercise(
-        name: 'Ejercicio 2', icon: Icons.shopping_bag, route: SecondScreen()),
+        name: 'Ejercicio 1',
+        icon: Icons.boy,
+        color: const Color.fromARGB(255, 9, 118, 219),
+        route: OneScreen()),
     Exercise(
-        name: 'Ejercicio 3', icon: Icons.architecture, route: ThirdScreen()),
+        name: 'Ejercicio 2',
+        icon: Icons.shopping_bag,
+        color: const Color.fromARGB(255, 134, 70, 132),
+        route: SecondScreen()),
     Exercise(
-        name: 'Ejercicio 4', icon: Icons.electric_bolt, route: FourthScreen()),
+        name: 'Ejercicio 3',
+        icon: Icons.architecture,
+        color: Colors.orange,
+        route: ThirdScreen()),
     Exercise(
-        name: 'Ejercicio 5', icon: Icons.attach_money, route: FifthScreen()),
-    Exercise(name: 'Ejercicio 6', icon: Icons.nature, route: SixthScreen()),
+        name: 'Ejercicio 4',
+        icon: Icons.electric_bolt,
+        color: Colors.yellow,
+        route: FourthScreen()),
+    Exercise(
+        name: 'Ejercicio 5',
+        icon: Icons.attach_money,
+        color: Colors.green,
+        route: FifthScreen()),
+    Exercise(
+        name: 'Ejercicio 6',
+        icon: Icons.nature,
+        color: const Color.fromARGB(255, 30, 111, 33),
+        route: SixthScreen()),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Ejercicios tarea 1'),
+        title: const Text('Tarea 1'),
       ),
       drawer: Drawer(
         child: ListView(
           padding: EdgeInsets.zero,
           children: <Widget>[
-            DrawerHeader(
+            const DrawerHeader(
               decoration: BoxDecoration(
                 color: Colors.blue,
               ),
@@ -61,8 +84,8 @@ class HomeScreen extends StatelessWidget {
               ),
             ),
             ListTile(
-              leading: Icon(Icons.info),
-              title: Text('Info'),
+              leading: const Icon(Icons.info),
+              title: const Text('Info'),
               onTap: () {
                 Navigator.push(
                   context,
@@ -74,9 +97,9 @@ class HomeScreen extends StatelessWidget {
         ),
       ),
       body: GridView.builder(
-        padding: EdgeInsets.all(16.0),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2, // Número de columnas en el grid
+        padding: const EdgeInsets.all(16.0),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
           crossAxisSpacing: 16.0,
           mainAxisSpacing: 16.0,
         ),
@@ -98,9 +121,14 @@ class HomeScreen extends StatelessWidget {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Icon(exer.icon, size: 50),
-                  SizedBox(height: 10),
-                  Text(exer.name, style: TextStyle(fontSize: 16)),
+                  Icon(
+                    exer.icon,
+                    size: 50,
+                    color:
+                        exer.color, // Usamos el color específico para el icono
+                  ),
+                  const SizedBox(height: 10),
+                  Text(exer.name, style: const TextStyle(fontSize: 16)),
                 ],
               ),
             ),
@@ -114,7 +142,12 @@ class HomeScreen extends StatelessWidget {
 class Exercise {
   final String name;
   final IconData icon;
+  final Color color; // Se añade un color a cada ejercicio
   final Widget route;
 
-  Exercise({required this.name, required this.icon, required this.route});
+  Exercise(
+      {required this.name,
+      required this.icon,
+      required this.color,
+      required this.route});
 }
