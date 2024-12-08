a88e1f655b28e1a4d51c93f8e3e616a1874bfae3
Josué Merino Calderón <yoshichicharra11@gmail.com>
2024-12-08 00:46:07 -0500

    Add exercise 1

--- deber3g1/lib/logic/ascii1.dart
+++ deber3g1/lib/logic/ascii1.dart
@@ -1,3 +1,10 @@
-class ascci1 {
-
+class ASCIIConverter {
+  /// Retorna el código ASCII de un carácter válido.
+  /// Si la entrada no es válida, lanza una excepción.
+  static int getASCIIValue(String input) {
+    if (input.isEmpty || input.length != 1) {
+      throw ArgumentError('Debes ingresar un solo carácter.');
+    }
+    return input.codeUnitAt(0);
+  }
 }
