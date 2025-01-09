b8914125caffbbf7a2710557e1aa687c10158da9
Josué Merino Calderón <yoshichicharra11@gmail.com>
2025-01-09 11:46:10 -0500

    Change the controller

--- segundo_parcial/giroscopio/giroscopio/lib/controllers/gyroscope_controller.dart
+++ segundo_parcial/giroscopio/giroscopio/lib/controllers/gyroscope_controller.dart
@@ -1,10 +1,10 @@
-import 'package:sensors_plus/sensors_plus.dart';
+import 'package:sensors/sensors.dart';
 import '../models/gyroscope_model.dart';
 
 class GyroscopeController {
   // Función para obtener los datos del giroscopio
   Stream<GyroscopeData> getGyroscopeData() {
-    return SensorsPlus.gyroscopeEvents.map((event) {
+    return gyroscopeEvents.map((event) {
       return GyroscopeData(x: event.x, y: event.y, z: event.z);
     });
   }
